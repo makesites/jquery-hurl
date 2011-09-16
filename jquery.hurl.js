f606b9c855c7f528a759bcb9bb8f2e89ddc5d1b6
Patrick D <patrickdaze@gmail.com>
2011-09-16 12:19:59 -0400

    Conventionalize the code
    - Using tabs
    - Combining vars into one var
    - Spaces between () when creating functions unless no variables are defined
    - Spaces between () for conditions unless no variables are defined
    - No spaces between () when calling a function

--- jquery.hurl.js
+++ jquery.hurl.js
@@ -17,249 +17,247 @@
  * $.hurl();
  * 
  * Update the hash URL with data queries, ex.
- * $.hurl("update", {"id": 15765});
+ * $.hurl('update', {'id': 15765});
  * 
  * Get the latest link by requesting 
- * $("body").data("hurl").link
+ * $('body').data('hurl').link
  * 
  * Update your page when the hash gets updated by using monitor:true 
  * and setting up an event listener, like this: 
- * $("body").bind('hurl', function(event) {
+ * $('body').bind('hurl', function(event) {
  *   // do something...
  * });
  * 
  */
- 
-(function( $ ){
-		  
-	$.hurl = function( method, options ){
-		$('body').hurl( method, options );
+
+(function( $ ) {
+
+	$.hurl = function( method, options ) {
+		$('body').hurl(method, options);
 	};
-	
-  //var hash = location.hash;
-  
-  var methods = {
-     init : function( options ) {
-    	
-		var settings = {
-		  'delimiter' : "/", 
-		  'initiator' : "!", 
-		  'pair' : true, 
-		  'monitor' : false 
-		};
-		
-		return this.each(function() {
 
-		var $this = $(this),
-			data = $this.data('hurl');
-			
-			// stop all operations if location.hash is not available in the browser
-			if( typeof location.hash != "undefined" ){
-				
-				// merging custom options with default settings
-				if ( options ) { 
-					$.extend( settings, options );
-				}
-				// save settings
-				$this.data('hurl', {settings: settings, link: {}});
-				
-				// parse hash uri
-				$this.hurl("parse");
-				
-				if( settings.monitor ){ 
+	var methods = {
+
+		init : function( options ) {
+
+			var settings = {
+				'delimiter' : '/',
+				'initiator' : '!',
+				'pair'      : true,
+				'monitor'   : false
+			};
+
+			return this.each(function() {
+				var $this = $(this),
+				    data = $this.data('hurl');
+
+				// stop all operations if location.hash is not available in the browser
+				if( typeof location.hash != 'undefined' ) {
+
+					// merging custom options with default settings
+					if ( options ) {
+						$.extend(settings, options);
+					}
+
+					// save settings
+					$this.data('hurl', {settings: settings, link: {}});
+
+					// parse hash uri
+					$this.hurl('parse');
+
 					// start monitoring the hash events
-					$this.hurl("monitor");
-				}
-				
-			} else {
-				return false;
-			}
-			
-    	});
-	
-	},
-	
-	// pase the URL on page load
-	parse : function( options ) { 
-		return this.each(function(){
+					if( settings.monitor ) {
+						$this.hurl('monitor');
+					}
 
-		var $this = $(this), 
-			data = $this.data('hurl'), 
-			settings = data.settings;
-			
-			var link = {};
-			var hash = location.hash;
-			
-			// see if there is an initiator (default: !)
-			if( hash.indexOf(settings.initiator) ){ 
-				// get the pure URI
-				var uri = hash.substr( hash.indexOf(settings.initiator)+1 );
-				var uri_parts = uri.split(settings.delimiter);
-			} 
-			
-			// FIX -- Remove first element if blank
-			if(uri_parts[0]=="") { uri_parts.shift() }
-			
-			// parse in key-value pairs
-			if( settings.pair ){
-				for(i in uri_parts){ 
-					//link[i] = uri_parts[i];
-					// odd numbers are keys, values are even numbers
-					if( i%2 ){ 
-						// register the value to the previous key
-						link[uri_parts[i-1]] = uri_parts[i];
-					} else {
-						// register the key
-						link[uri_parts[i]] = 1;
+				} else {
+					return false;
+				}
+
+			});
+		},
+
+		// pase the URL on page load
+		parse : function( options ) { 
+			return this.each(function(){
+
+				var $this    = $(this),
+				    data     = $this.data('hurl'),
+				    settings = data.settings,
+				    link     = {},
+				    hash     = location.hash;
+
+				// see if there is an initiator (default: !)
+				if( hash.indexOf(settings.initiator) ) {
+					// get the pure URI
+					var uri       = hash.substr(hash.indexOf(settings.initiator)+1);
+					var uri_parts = uri.split(settings.delimiter);
+				} 
+
+				// Remove first element if blank
+				if( uri_parts[0]=='' ) {
+					uri_parts.shift();
+				}
+
+				// parse in key-value pairs
+				if( settings.pair ) {
+					for(i in uri_parts) {
+						// odd numbers are keys, values are even numbers
+						if( i%2 ) {
+							// register the value to the previous key
+							link[uri_parts[i-1]] = uri_parts[i];
+						} else {
+							// register the key
+							link[uri_parts[i]] = 1;
+						}
+					}
+				} else {
+					for(i in uri_parts) {
+						link[i] = uri_parts[i];
 					}
 				}
-			} else {
-				for(i in uri_parts){ 
-					link[i] = uri_parts[i];
-				}
-			}
-			
-			// merge with the existing link object
-			$.extend(data.link, link);
-			
-			// save url for future reference
-			$this.data('hurl', data);
-			
-			
-		});
-	},
-	
-	monitor : function( options ) { 
-		return this.each(function(){
 
-		var $this = $(this), 
-			data = $this.data('hurl');
-			
-			// save the current link
-			var url = methods.create( $this );
-			
-			// get the new link
-			if ( url != location.hash ) { 
-				$this.hurl("parse");
-				// trigger the update for all scripts listening
-				$this.trigger("hurl", "auto");
-			}
-			
-			// use settimeout for now...
-			setTimeout( function(){ $this.hurl("monitor") }, 500);
+				// merge with the existing link object
+				$.extend(data.link, link);
 
-		});
-	},
-	
-	request : function( link ) { 
-		return this.each(function(){
-
-		var $this = $(this),
-			data = $this.data('hurl');
-			
-			if ( link ) { 
-				data.link = link;
-				
-				// save url
+				// save url for future reference
 				$this.data('hurl', data);
-				
-				// register click
-				$this.hurl("register");
-				
-				// trigger the update for all scripts listening
-				$this.trigger("hurl", "manual");
-			}
-						
-		});
-	},
-	
-	update : function( link ) { 
-		return this.each(function(){
 
-		var $this = $(this),
-			data = $this.data('hurl');
-			
-			if ( link ) { 
-				$.extend( data.link, link );
-				
-				// save url
-				$this.data('hurl', data);
-				
-				// register click
-				$this.hurl("register");
-			}
-			
-			
-		});
-	},
-	
-	register : function( ) { 
-		return this.each(function(){
-
-		var $this = $(this), 
-			data = $this.data('hurl');
-			
-			// stingify link
-			var url = methods.create( $this );
-			// register the click in the browser's history
-			$a = $('<a><!-- --></a>').attr("href", url).css({
-				position:'absolute',
-				top: $(window).scrollTop(),
-				left: $(window).scrollLeft()
 			});
-			$('body').prepend($a);
-			location = url;
-			$a.remove();
-						
-		});
-	},
-	
-	destroy : function( ) { 
-		return this.each(function(){
+		},
 
-		var $this = $(this),
-             data = $this.data('hurl');
-			// unbind the mouse scroll
-			// * condition? 
-			data.hurl.remove();
-         	$this.removeData('hurl');
+		monitor : function( options ) {
+			return this.each(function() {
 
-		});
-	},
-	// helper functions
-	create : function( $this ) { 
+				var $this = $(this), 
+				    data  = $this.data('hurl');
+
+				// save the current link
+				var url = methods.create($this);
+
+				// get the new link
+				if ( url != location.hash ) { 
+					$this.hurl('parse');
+					// trigger the update for all scripts listening
+					$this.trigger('hurl', 'auto');
+				}
+
+				// use setTimeout for now...
+				setTimeout(function(){ $this.hurl('monitor') }, 500);
+
+			});
+		},
+
+		request : function( link ) {
+			return this.each(function() {
+
+				var $this = $(this),
+				    data  = $this.data('hurl');
+
+				if ( link ) {
+					data.link = link;
+
+					// save url
+					$this.data('hurl', data);
+
+					// register click
+					$this.hurl('register');
+
+					// trigger the update for all scripts listening
+					$this.trigger('hurl', 'manual');
+				}
+
+			});
+		},
+
+		update : function( link ) {
+			return this.each(function(){
+
+				var $this = $(this),
+				    data  = $this.data('hurl');
+
+				if ( link ) {
+					$.extend(data.link, link);
+
+					// save url
+					$this.data('hurl', data);
+
+					// register click
+					$this.hurl('register');
+				}
+
+			});
+		},
+
+		register : function() { 
+			return this.each(function() {
+
+				var $this = $(this), 
+				    data  = $this.data('hurl');
+
+				// stingify link
+				var url = methods.create($this);
+				
+				// register the click in the browser's history
+				$a = $('<a><!-- --></a>').attr('href', url).css({
+				    position: 'absolute',
+				    top:      $(window).scrollTop(),
+				    left:     $(window).scrollLeft()
+				});
+				$('body').prepend($a);
+				location = url;
+				$a.remove();
+
+			});
+		},
+
+		destroy : function( ) { 
+			return this.each(function(){
+
+				var $this = $(this),
+				    data  = $this.data('hurl');
+
+				// unbind the mouse scroll
+				// * condition? 
+				data.hurl.remove();
+				$this.removeData('hurl');
+
+			});
+		},
 		
-		var data = $this.data('hurl'),
-			settings = data.settings;
-			
-			var url = "#"+settings.initiator;
-			
+		// helper functions
+		create : function( $this ) { 
+
+			var data     = $this.data('hurl'),
+			    settings = data.settings,
+			    url      = '#' + settings.initiator;
+
 			// parse in key-value pairs
-			if( settings.pair ){
-				for(i in data.link){
+			if( settings.pair ) {
+				for(i in data.link) {
 					url += settings.delimiter + i + settings.delimiter + data.link[i];
 				}	
 			} else {
-				for(i in data.link){
+				for(i in data.link) {
 					url += settings.delimiter + data.link[i];
 				}
 			}
+
+			return url;
+		}
+
+	};
+
+	$.fn.hurl = function( method ) {
+
+		if ( methods[method] ) {
+			return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+		} else if ( typeof method === 'object' || ! method ) {
+			return methods.init.apply(this, arguments);
+		} else {
+			$.error('Method ' +  method + ' does not exist on jQuery.hurl');
+		}
 			
-		return url;
-	}
-	
-  };
+	};
 
-  $.fn.hurl = function( method ) {
-    
-    if ( methods[method] ) {
-      return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
-    } else if ( typeof method === 'object' || ! method ) {
-      return methods.init.apply( this, arguments );
-    } else {
-      $.error( 'Method ' +  method + ' does not exist on jQuery.hurl' );
-    }    
-  
-  };
-
-})( jQuery );
+})(jQuery);
